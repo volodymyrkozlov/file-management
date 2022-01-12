430dcdbc6f4f0db9272abc83615c4fbb12b5b4dc
Volodymyr Kozlov <volodymyr.kozlow@gmail.com>
2022-01-12 14:54:54 +0200

    initial commit

---
+++ file-management-service/src/test/java/com/volodymyrkozlov/filemanagement/app/FileManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volodymyrkozlov.filemanagement.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
